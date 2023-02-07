35d5f11273e4a71edbfec4b1f15f1cf772c78a20
Mariia Akymenko <dafnarianna@gmail.com>
2023-02-07 18:35:39 +0200

    make tasks

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statsList}>
+        {stats.map(stat => (
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+// export function randomColor() {
+//   Math.floor(Math.random() * 16777215).toString(16);
+// }
